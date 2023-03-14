8822dc1af8cf0c3eb5c42cffdf18ea4ff72ab649
TonyTech-GIT <toomgbebu@gmail.com>
2023-03-14 11:09:17 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Parent from "./components/Parent";
+// Houses the parent component which contains the child component...
+function App() {
+  return (
+    <div className="App">
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
